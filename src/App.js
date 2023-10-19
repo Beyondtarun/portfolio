574dfc5ea7bc487d189f22e3799aee6a8accdb4b
Beyondtarun <teg.tarun@gmail.com>
2023-10-19 20:41:09 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import tarun from './images/tarun.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <p>
+         Tarun Goyal's Portfolio...
         </p>
+        <img src={tarun} className="App-logo" alt="logo" />
+        
+        <div className='SocialLinks'>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/beyondtarun/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LinkedIn
         </a>
+        </div>
+
+        
       </header>
     </div>
   );
